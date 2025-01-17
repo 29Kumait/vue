ba602c99f159ab7783389f31666dc7f39daf3302
kumait <Kumait@gmail.com>
2025-01-17 23:38:31 +0100

    # Commented out incomplete files.
     `error TS2307: Cannot find module './router' or its corresponding type declarations.`

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,15 @@ import './style.css'
 import App from './App.vue'
 
 import { createPinia } from 'pinia'
-import { router } from './router'
-import clickOutside from './directives/click-outside'
+// import { router } from './router'
+// import clickOutside from './directives/click-outside'
 
 const pinia = createPinia()
 const app = createApp(App)
 
 
-app.directive('click-outside', clickOutside)
+// app.directive('click-outside', clickOutside)
 
-app.use(router)
+// app.use(router)
 app.use(pinia)
 app.mount('#app')
