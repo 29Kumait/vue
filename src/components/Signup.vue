23e422fc7676c8533a70165c7dc34f0ccc3f4d4a
kumait <Kumait@gmail.com>
2025-01-25 01:01:30 +0100

    Update dependencies and enhance UI components for improved user experience

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -13,16 +13,15 @@ function handleSignUp() {
 
 
 <template>
-  <div class="max-w-md mx-auto">
+  <div class="max-w-md mx-auto px-6 py-12">
     <div class="mb-4">
       <label class="block text-sm font-medium text-gray-700">Email</label>
       <input type="email" v-model="email" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
     </div>
     <div class="mb-4">
-      <label  class="block text-sm font-medium text-gray-700">Password</label>
-      <input  type="password" v-model="password" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+      <input  placeholder="password"  type="password" v-model="password" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
     </div>
-    <button class="button block w-full px-4 py-2 bg-neon-cyan text-white font-semibold rounded-md shadow-sm hover:bg-neon-indigo focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-a-cyan" @click="handleSignUp" :disabled="loading">Sign Up </button>
+    <button class="button block w-full px-4 py-2 bg-p3-pink text-white font-semibold rounded-md shadow-sm hover:bg-neon-indigo focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-a-cyan" @click="handleSignUp" :disabled="loading">Sign Up </button>
     <p v-if="errorMsg" class="error">{{ errorMsg }}</p>
   </div>
 </template>
