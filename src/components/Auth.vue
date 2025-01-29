4407723435db8313ada6aaca539060c8d5f5f356
Kumait <137179507+29Kumait@users.noreply.github.com>
2025-01-29 01:31:40 +0100

    Update Auth.vue

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useAuth } from '../composables/useAuth'
+import { ref } from 'vue';
+import { useAuth } from '../composables/useAuth';
 
-const email = ref('')
-const password = ref('')
-const { loading, errorMsg, signIn } = useAuth()
+const email = ref('');
+const password = ref('');
+const { loading, errorMsg, signIn } = useAuth();
 
 function handleSignIn() {
-  signIn(email.value, password.value)
+  signIn(email.value, password.value);
 }
 </script>
 
@@ -15,12 +15,27 @@ function handleSignIn() {
   <div class="max-w-md mx-auto px-6 py-12">
     <div class="mb-4">
       <label class="block text-sm font-medium text-gray-700">Email</label>
-      <input type="email" v-model="email" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" />
+      <input
+        type="email"
+        v-model="email"
+        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+      />
     </div>
     <div class="mb-4">
-      <input placeholder="password" v-model="password" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm bg-transparent" type="password"/>
+      <label class="block text-sm font-medium text-gray-700">Password</label>
+      <input
+        type="password"
+        v-model="password"
+        class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+      />
     </div>
-      <button class="button block w-full px-4 py-2 text-p3-light font-semibold rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" @click="handleSignIn" :disabled="loading">Sign In</button>
-        <p v-if="errorMsg" class="error">{{ errorMsg }}</p>
+    <button
+      class="button block w-full px-4 py-2 text-white bg-indigo-600 font-semibold rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      @click="handleSignIn"
+      :disabled="loading"
+    >
+      Sign In
+    </button>
+    <p v-if="errorMsg" class="error">{{ errorMsg }}</p>
   </div>
 </template>
