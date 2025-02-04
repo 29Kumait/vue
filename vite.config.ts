cbbb301f3ca2a0fd0d29a9b260200b35e4ff02bd
Kumait <137179507+29Kumait@users.noreply.github.com>
2025-02-04 18:28:50 +0100

    Remove `helmet` import from `vite.config.ts` and add `helmet` to `dependencies` in `package.json`
    * **vite.config.ts**
      - Remove `helmet` import
      - Remove `helmet` from plugins array
    
    * **package.json**
      - Add `helmet` to `dependencies` with version `^6.0.0`
    
    To fix deployment failure and err :
    
    
    ```
    error during build:
    Error [ERR_MODULE_NOT_FOUND]: Cannot find package 'helmet' imported from /vercel/path0/node_modules/.vite-temp/vite.config.ts.timestamp-1738689962058-b45da302ff823.mjs
        at packageResolve (node:internal/modules/esm/resolve:857:9)
        at moduleResolve (node:internal/modules/esm/resolve:926:18)
        at defaultResolve (node:internal/modules/esm/resolve:1056:11)
        at ModuleLoader.defaultResolve (node:internal/modules/esm/loader:654:12)
        at #cachedDefaultResolve (node:internal/modules/esm/loader:603:25)
        at ModuleLoader.resolve (node:internal/modules/esm/loader:586:38)
        at ModuleLoader.getModuleJobForImport (node:internal/modules/esm/loader:242:38)
        at ModuleJob._link (node:internal/modules/esm/module_job:135:49)
    Error: Command "npm run build" exited with 1
    
    ```

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import viteCompression from 'vite-plugin-compression'
-import helmet from 'helmet'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
-    viteCompression(),
-    helmet()
+    viteCompression()
   ],
 
   ssr: {
