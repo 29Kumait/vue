662cf0f5e0930ebfa73dde61d5dad2459000a661
Kumait <137179507+29Kumait@users.noreply.github.com>
2025-02-07 16:33:48 +0100

    feat: add vite-plugin-html and vite-plugin-vue-devtools to enhance development experience

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite';
 
+import { createHtmlPlugin } from 'vite-plugin-html'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    tailwindcss()
+    tailwindcss(),
+    vueDevTools(),
+    createHtmlPlugin({}),
   ],
 
   ssr: {
