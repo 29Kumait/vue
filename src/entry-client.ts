25cb266442e0453bdb792d2073474581890bbe74
Kumait <137179507+29Kumait@users.noreply.github.com>
2025-02-07 14:09:40 +0100

    fix: update Pinia state type and improve hydration logic

--- src/entry-client.ts
+++ src/entry-client.ts
@@ -4,7 +4,7 @@ import './style.css'
 const { app, router, pinia } = createApp(false)
 
 // Hydrate Pinia from SSR if you inserted state into window.__PINIA
-if (window.__PINIA) pinia.state.value = JSON.parse(window.__PINIA)
+if (window.__PINIA) pinia.state.value = (window.__PINIA)
 
 router.isReady().then(() => {
     // Hydrate the SSR-generated HTML
